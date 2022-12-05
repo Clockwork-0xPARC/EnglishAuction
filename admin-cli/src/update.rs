ce34bddf12afa7080460ac71b9450d8ff98cdb5c
John Detter <4099508+jdetter@users.noreply.github.com>
2022-12-05 11:30:24 +0000

    Set num matches and players (#9)
    * Set number of matches and players
    
    * Cargo fix
    
    * Working
    
    Co-authored-by: Boppy <no-reply@boppygames.gg>

---
+++ admin-cli/src/update.rs
@@ -0,0 +1,36 @@
+use clap::{Arg, ArgMatches};
+use clap;
+use duct::cmd;
+
+pub fn cli() -> clap::Command {
+    clap::Command::new("update")
+        .about("Updates the configuration of an initialized game.")
+        .after_help("Run `english-auction update --help` for more detailed information.\n`")
+        .arg(Arg::new("matches")
+                .long("matches")
+                .short('m')
+                .required(false)
+                .value_parser(clap::value_parser!(u32))
+                .help("The number of matches to play in a tournament"))
+        .arg(Arg::new("players")
+                .long("players")
+                .short('p')
+                .required(false)
+                .value_parser(clap::value_parser!(u32))
+                .help("The number of players to allow to register in the tournament"))
+}
+
+pub async fn exec(args: &ArgMatches) -> Result<(), anyhow::Error> {
+    let matches = args.get_one::<u32>("matches");
+    let players = args.get_one::<u32>("players");
+
+    if let Some(matches) = matches {
+        cmd!("spacetime", "call", "english-auction", "set_num_matches", format!("[{}]", matches)).run()?;
+    }
+
+    if let Some(players) = players {
+        cmd!("spacetime", "call", "english-auction", "set_max_players", format!("[{}]", players)).run()?;
+    }
+
+    Ok(())
+}
