ead5c61a231dbc74205a5a5cc97cf395092c024e
Tyler Cloutier <cloutiertyler@aol.com>
2022-12-06 16:29:39 +0000

    Updated word list

--- typescript-sdk/filter.ts
+++ typescript-sdk/filter.ts
@@ -5,7 +5,7 @@ const words: string[] = JSON.parse(fs.readFileSync("../words.json").toString("ut
 const shortWords = [];
 let i = 0;
 for (const word of words) {
-  if (word.length > 2 && /[aeiouy]/i.test(word)) {
+  if (word.length > 2 && /[aeiouy]/i.test(word) && word.length <= 9) {
     i++;
     shortWords.push(word);
   }
